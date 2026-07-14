750347a57aa866ae17635f0e0c3e7a6f850cb278
fttlov <154821035+fttlov@users.noreply.github.com>
2026-07-14 14:16:41 +0300

    Add files via upload

--- JoyShockLibrary/JoyShockLibrary.h
+++ JoyShockLibrary/JoyShockLibrary.h
@@ -229,6 +229,7 @@ extern "C" JOY_SHOCK_API void JslStartContinuousCalibration(int deviceId);
 extern "C" JOY_SHOCK_API void JslPauseContinuousCalibration(int deviceId);
 extern "C" JOY_SHOCK_API void JslSetAutomaticCalibration(int deviceId, bool enabled);
 extern "C" JOY_SHOCK_API void JslSetStillnessSettings(int deviceId, float maxError, float minCollectionTime, float minCorrectionTime, float easeInTime);	//@502
+extern "C" JOY_SHOCK_API void JslSetGravitySettings(int deviceId, float shakinessMin, float shakinessMax, float stillSpeed, float shakySpeed);		//@502
 extern "C" JOY_SHOCK_API void JslGetCalibrationOffset(int deviceId, float& xOffset, float& yOffset, float& zOffset);
 extern "C" JOY_SHOCK_API void JslSetCalibrationOffset(int deviceId, float xOffset, float yOffset, float zOffset);
 extern "C" JOY_SHOCK_API JSL_AUTO_CALIBRATION JslGetAutoCalibrationStatus(int deviceId);
